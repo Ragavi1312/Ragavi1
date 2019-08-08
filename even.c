dbbbd9498fe074204ad9b042e5fef5bccd73c956
Ragavi1312 <53895871+Ragavi1312@users.noreply.github.com>
2019-08-08 18:11:48 +0530

    Update and rename README.md to even.c

---
+++ even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n%2==0)
+	printf("A num is even");
+	else if(n%2!=0)
+	printf("A num is odd");
+	else
+	printf("invalid");
+	return 0;
+}
